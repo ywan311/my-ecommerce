425772e0fcf1b63e6d95675fbbeb0aba83a60bac
kimyoowan <ywan311@naver.com>
2020-05-27 20:49:15 +0900

    change environment

--- src/main/java/com/example/myecommerce/Domain/Product/Product.java
+++ src/main/java/com/example/myecommerce/Domain/Product/Product.java
@@ -3,6 +3,7 @@ package com.example.myecommerce.Domain.Product;
 import com.example.myecommerce.Domain.BaseTimeEntity;
 import com.example.myecommerce.Domain.Category.Category;
 import com.example.myecommerce.Domain.Comment.Comment;
+import com.example.myecommerce.Domain.ImageFile.ImageFile;
 import com.example.myecommerce.Domain.Order.Order;
 import com.example.myecommerce.Domain.User.User;
 import com.fasterxml.jackson.annotation.JsonBackReference;
@@ -13,7 +14,9 @@ import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 import javax.persistence.*;
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 @Getter
@@ -51,6 +54,10 @@ public class Product extends BaseTimeEntity {
     @OneToMany(mappedBy = "product")
     @JsonManagedReference
     private Set<Order> orders = new HashSet<>();
+
+    @OneToMany(mappedBy = "product")
+    @JsonManagedReference
+    private List<ImageFile> files = new ArrayList<>();
 
 
     @Builder
--- src/main/java/com/example/myecommerce/MyEcommerceApplication.java
+++ src/main/java/com/example/myecommerce/MyEcommerceApplication.java
@@ -5,10 +5,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @EnableConfigurationProperties({FileUploadProperties.class})
 @EnableJpaAuditing
 @SpringBootApplication
+@EnableWebMvc
 public class MyEcommerceApplication {
 
     public static void main(String[] args) {
