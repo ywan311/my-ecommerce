4dd8d66aaa9865a0a84337c39f718c5a4482ef98
yoowankim <ywan311@naver.com>
2020-07-10 16:50:25 +0900

    jenkins start

--- src/main/java/com/example/myecommerce/MyEcommerceApplication.java
+++ src/main/java/com/example/myecommerce/MyEcommerceApplication.java
@@ -8,6 +8,8 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @SpringBootApplication
 public class MyEcommerceApplication {
 
+
+
     public static void main(String[] args) {
         SpringApplication.run(MyEcommerceApplication.class, args);
     }
