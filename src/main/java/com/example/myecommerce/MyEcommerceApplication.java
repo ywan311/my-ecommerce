4fb5f6e36be0054cd3cb891e85355d40a156d629
yoowankim <ywan311@naver.com>
2021-06-06 16:13:55 +0900

    feat: Aop setting AOP 설정 및 기본 로거 추가

--- src/main/java/com/example/myecommerce/MyEcommerceApplication.java
+++ src/main/java/com/example/myecommerce/MyEcommerceApplication.java
@@ -2,9 +2,11 @@ package com.example.myecommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+@EnableAspectJAutoProxy
 @EnableJpaAuditing
 @SpringBootApplication
 public class MyEcommerceApplication {
